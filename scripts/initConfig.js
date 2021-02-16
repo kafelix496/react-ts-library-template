fd2aeaf280109336cbdf64911ec173f7d07a6aa0
Felix J Lee <ka.felix496@gmail.com>
2021-02-15 21:16:53 -0500

    Beautify code

--- scripts/initConfig.js
+++ scripts/initConfig.js
@@ -3,36 +3,43 @@ const fs = require('fs')
 const { prompt } = require('enquirer')
 const { execSync } = require('child_process')
 
-console.log('===== Package information... If you want to skip question, please press enter =====')
-prompt([{
-  type: 'input',
-  name: 'name',
-  message: 'Input package name : '
-}, {
-  type: 'input',
-  name: 'description',
-  message: 'Input package description : '
-}, {
-  type: 'input',
-  name: 'author',
-  message: 'Input package author : '
-}, {
-  type: 'list',
-  name: 'keywords',
-  message: 'Input package keywords ( Type comma-separated keywords ) : '
-}, {
-  type: 'input',
-  name: 'umdName',
-  message: 'Input umd global variable name : '
-}]).then((answer) => {
+console.log(
+  '===== Package information... If you want to skip question, please press enter ====='
+)
+prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Input package name : '
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Input package description : '
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'Input package author : '
+  },
+  {
+    type: 'list',
+    name: 'keywords',
+    message: 'Input package keywords ( Type comma-separated keywords ) : '
+  },
+  {
+    type: 'input',
+    name: 'umdName',
+    message: 'Input umd global variable name : '
+  }
+]).then((answer) => {
   // console.log('answer', answer)
 
   try {
-    const rollupConfigFile = fs.readFileSync('rollup.config.js', 'utf8')
+    const rollupConfigFileContent = fs.readFileSync('rollup.config.js', 'utf8')
     fs.writeFileSync(
       'rollup.config.js',
-      rollupConfigFile
-        .replace(/CUSTOM_UMD_GLOBAL_VARIABLE/g, answer.umdName)
+      rollupConfigFileContent.replace(/CUSTOM_UMD_GLOBAL_VARIABLE/g, answer.umdName)
     )
 
     const packageJsonString = fs.readFileSync('package.json', 'utf8')
